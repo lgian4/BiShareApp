528b7e2cc0eeee87a28eceae3bfd485894ed58cc
Linargian Pratama <linargian00@gmail.com>
2021-03-26 16:39:36 +0700

    install package

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RegisterScreen from './view/RegisterScreen';
 
-export default function App() {
+const RegisterStack = createStackNavigator();
+const AuthStack = createStackNavigator();
+const RegisterTab = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RegisterStack.Navigator
+      screenOptions={{ gestureEnabled: false, headerShown: false }}
+    >
+
+
+      <RegisterStack.Screen
+        name="Register"
+        component={RegisterScreen}
+      />
+
+    </RegisterStack.Navigator>
   );
+}
+
+
+
+export default class App extends React.Component {
+
+
+  constructor() {
+    super()
+
+ 
+    this.state = {
+      tokenUser: "",
+      tokenExpire: "",
+    }
+  }
+
+ 
+
+  render() {
+
+    return (
+      <NavigationContainer>
+        <AuthStack.Navigator
+          screenOptions={{ gestureEnabled: false, headerShown: false }}
+          
+        >
+          <AuthStack.Screen
+              name="splash"
+              component={RegisterTab}
+            />
+
+
+
+
+
+
+        </AuthStack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
